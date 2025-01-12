d1aed0ab5e979c9c2cef0f6757d819ed4f82fc42
Kyle553 <elkorda204@gmail.com>
2025-01-12 05:15:09 +0200

    Correction of task 8

--- FD_8_max_of_3/FD_8_max_of_3.pas
+++ FD_8_max_of_3/FD_8_max_of_3.pas
@@ -4,35 +4,34 @@ var
   num1: integer;
   num2: integer;
   num3: integer;
+  maxnum: integer;
+
 
 begin
   writeln('Введіть 3 числа:');
   readln(num1);
   readln(num2);
   readln(num3);
-  
-  if (num1 >= num2 + num3) then
-    writeln('Найбільше перше число: ', num1)
-  else 
-    if (num2 >= num1 + num3) then
-      writeln('Найбільше друге число: ', num2)
-  else 
-    if (num3 >= num1 + num2) then
-      writeln('Найбільше третє число: ', num3)
-  else
-    writeln('Всі три числа однинакові');
-  
-  
-  (*
-  
-  if (num1 >= num2) and (num1 >= num3) then
-    max_num := num1
-  else if (num2 >= num1) and (num2 >= num3) then
-    max_num := num2
-  else
-    max_num := num3;
 
-  writeln('Найбільше число: ', max_num);
-  
-  *)
+  if num1 >= num2 then
+    maxnum := num1
+  else
+    if num1 >= num3 then
+      maxnum := num1
+    else
+      if num2 >= num1 then
+        maxnum := num2
+      else
+        if num2 >= num3 then
+          maxnum := num2
+        else
+          if num3 >= num1 then
+            maxnum :=num3
+          else
+            if num3 >= num2 then
+              maxnum := num3;
+        
+      
+
+  writeln('Найбільше число: ', maxnum);
 end.
