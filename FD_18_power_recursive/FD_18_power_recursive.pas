763f94952da3ecb2f13d4e795f82c613a57bd9c2
Kyle553 <elkorda204@gmail.com>
2025-01-26 20:35:20 +0200

    homework

---
+++ FD_18_power_recursive/FD_18_power_recursive.pas
@@ -0,0 +1,28 @@
+﻿program FD_18_power_recursive;
+
+function recursive(r1: integer; r2: integer): integer;
+  
+begin
+  if r2 = 0 then
+    result := 1
+  else 
+    result := r1 * recursive(r1, r2 - 1);
+end;
+
+var
+  num1: integer;
+  num2: integer;
+  res: integer;
+  
+begin
+  num1 := 0;
+  num2 := 0;
+  
+  writeln('Введіть число: ');
+  readln(num1);
+  writeln('Введіть степінь до якого буде піднесено число: ');
+  readln(num2);
+  
+  res := recursive(num1, num2);
+  writeln(res);
+end.
